af2540a7f689b8a5dd97d840f5b9cd791b6ee42d
Dushyant Bhardwaj <dushyantbha012@gmail.com>
2024-05-19 20:40:59 +0530

    created database and basic routes

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@/components/ui/button";
-export default function Home() {
-  return (
-    <div className="flex flex-col">
-      <div className="text-3xl font-bold text-indigo-500">hello, discord</div>
-      <Button>Click me</Button>
-    </div>
-  );
-}
