9ccdaa9ed12fa5e4904756e1f2e9c17d6f06de7a
Dushyant Bhardwaj <dushyantbha012@gmail.com>
2024-05-20 12:13:18 +0530

    added side bar and servers

--- components/navigation/navigation-action.tsx
+++ components/navigation/navigation-action.tsx
@@ -6,10 +6,10 @@ const NavigationAction = async () => {
     <div>
       <ActionTooltip side="right" align="center" label="Add a server">
         <button className="group flex items-center">
-          <div className="flex mx-3 h-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden items-center justify-center bg-background dark:bg-neutral-700 group-hover:bg-emerald-500">
+          <div className="flex mx-3 h-[48px] w-[48px] rounded-full group-hover:rounded-[16px] transition-all overflow-hidden items-center justify-center bg-background dark:bg-neutral-700 group-hover:bg-emerald-500">
             <Plus
-              className="group-hover:text-white transition text-emerald-500 mx-3"
-              size={25}
+              className="group-hover:text-white transition text-emerald-500"
+              size={20}
             />
           </div>
         </button>
---
+++ components/navigation/navigation-item.tsx
@@ -0,0 +1,44 @@
+"use client";
+import Image from "next/image";
+import { useParams, useRouter } from "next/navigation";
+import { cn } from "@/lib/utils";
+import { ActionTooltip } from "@/components/ui/action-tooltip";
+
+interface NavigationItemProps {
+  id: String;
+  imageUrl: string;
+  name: string;
+}
+
+export const NavigationItem = ({ id, imageUrl, name }: NavigationItemProps) => {
+  const params = useParams();
+  const router = useRouter();
+  console.log("id is ", id, "image url is", imageUrl, "name is ", name);
+  return (
+    <ActionTooltip side="right" align="center" label={name}>
+      <button
+        onClick={() => {
+          router.push(`servers/${id}`);
+        }}
+        className="group relative flex items-center mt-6"
+      >
+        <div
+          className={cn(
+            "relative group flex mx-3 h-[48px] w-[48px] rounded-full group-hover:rounded-[16px] transition-all overflow-hidden ",
+            params?.serverId === id &&
+              "bg/primary/10 text-primary rounded-[16px]"
+          )}
+        >
+          <Image src={imageUrl} alt="Channel" height="48" width="48" />
+        </div>
+        <div
+          className={cn(
+            "absolute left-0 bg-primary rounded-r-full transition-all w-[4px]",
+            params?.serverId !== id && "group-hover:h-[20px]",
+            params?.serverId === id ? "h-[48px]" : "h-[16px]"
+          )}
+        ></div>
+      </button>
+    </ActionTooltip>
+  );
+};
