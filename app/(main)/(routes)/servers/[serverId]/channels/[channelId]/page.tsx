69c292f5218056810d029b415a6d154d00bdf66c
Dushyant Bhardwaj <dushyantbha012@gmail.com>
2024-05-29 20:02:59 +0530

    added chatheader and mobile toggle feature

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,5 +1,39 @@
-function ChannelIdPage() {
-  return <div>ChannelIdPage</div>;
+import ChatHeader from "@/components/chat/chat-header";
+import { currentProfile } from "@/lib/currentProfile";
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+interface ChannelIdPageProps {
+  params: {
+    serverId: string;
+    channelId: string;
+  };
+}
+
+async function ChannelIdPage({ params }: ChannelIdPageProps) {
+  const profile = await currentProfile();
+  if (!profile) auth().redirectToSignIn();
+  const channel = await db.channel.findUnique({
+    where: { id: params.channelId },
+  });
+
+  const member = await db.member.findFirst({
+    where: { serverId: params.serverId, profileId: profile?.id },
+  });
+
+  if (!channel || !member) {
+    redirect("/");
+  }
+  return (
+    <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+      <ChatHeader
+        name={channel.name}
+        serverId={channel.serverId}
+        type="channel"
+      />
+    </div>
+  );
 }
 
 export default ChannelIdPage;
